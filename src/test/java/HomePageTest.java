a64c40b170663d580b1c5008aa668358ca788e18
NinaFleur <mamaeva91@mail.ru>
2018-11-08 23:38:22 +0200

    - adding a vary of WebDriver methods

--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -1,4 +1,6 @@
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
@@ -8,6 +10,7 @@ import org.testng.annotations.Test;
 import pageobject.HomePage;
 import pageobject.SearchResultPage;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class HomePageTest {
@@ -16,24 +19,35 @@ public class HomePageTest {
 
     @BeforeMethod
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "/Users/nina/Automation/projects/autoportal/src/main/resources/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/Users/mac/Automation/projects/autoportal/src/main/resources/chromedriver");
         driver = new ChromeDriver();
         PageFactory.initElements(driver, this);
+        String description = driver.getWindowHandle();
+        System.out.println(description);
+        System.out.println();
         driver.manage().window().fullscreen();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://autoportal.com/");
+        driver.navigate().refresh();
+        String url = driver.getCurrentUrl();
+        System.out.println(url);
     }
 
     @Test(priority = 0)
     public void verifyHeaderLinks() {
         new HomePage(driver)
                 .compareHeaderLinks();
+        List links = driver.findElements(By.tagName("li"));
+        System.out.println(links.size());
+
     }
 
     @Test
     public void verifyCarFormTitle(){
         new HomePage(driver)
                 .verifyTitle();
+        String title = driver.getTitle();
+        System.out.println(title);
     }
 
 
@@ -43,11 +57,13 @@ public class HomePageTest {
                 .searchForQuery("Tata");
         new SearchResultPage(driver)
                 .verifyResults();
+        String pagesource = driver.getPageSource();
+        System.out.println(pagesource);
     }
 
     @AfterMethod
     public void closeDriver() {
-        driver.quit();
+        driver.close();
     }
 
 
