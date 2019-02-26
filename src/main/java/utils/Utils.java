83a698a3541829f989dc2cc49f9e1ca3b5aa3157
NinaFleur <mamaeva91@mail.ru>
2019-02-26 18:59:32 +0200

    - added WriteReadFileTest, storePageTitlesInFile method

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -1,10 +1,21 @@
 package utils;
 
+import org.apache.log4j.Logger;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 public class Utils {
+
+    protected static final Logger LOGGER = LoggerUtil.getInstance();
+
+
+    private Utils() {
+    }
 
     public static String getRandomString(int length) {
         final String neededChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -29,5 +40,23 @@ public class Utils {
                 "Hyderabad", "Pune", "Chennai", "Ahmedabad", "Mumbai", "Surat", "Gurgaon", "Faridabad", "Gurgaon", "Ghaziabad");
         Random rand = new Random();
         return givenLocationList.get(rand.nextInt(givenLocationList.size()));
+    }
+
+    public static void storePageTitlesInFile(String pageTitles) {
+        LOGGER.info("Create file.txt and write data into it");
+        String fileName = "PageTitles.txt";
+        try {
+            File file = new File("src/test/resources/" + fileName);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(pageTitles);
+            file.getAbsolutePath();
+            bw.close();
+        } catch (Exception e) {
+            LOGGER.error(e);
+        }
     }
 }
---
+++ src/test/java/WriteReadFileTest.java
@@ -0,0 +1,53 @@
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pageobjects.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
+import static utils.Utils.storePageTitlesInFile;
+
+public class WriteReadFileTest extends BaseTest {
+
+    private HomePage homePage;
+    private BrandPage brandPage;
+    private BrochurePage brochurePage;
+    private OnRoadPricePage onRoadPricePage;
+    private ModelPage modelPage;
+
+    @Override
+    @BeforeMethod
+    public void beforeMethodInit() {
+        super.beforeMethodInit();
+        homePage = new HomePage();
+        brandPage = new BrandPage();
+        brochurePage = new BrochurePage();
+        onRoadPricePage = new OnRoadPricePage();
+        modelPage = new ModelPage();
+    }
+
+    @Test(description = "Get 5 page titles and save them to the file.txt")
+    public void getPageTitles() {
+        LOGGER.info("Collecting titles of five pages.");
+        List<String> pageTitles = new ArrayList<>();
+        homePage.navigate();
+        pageTitles.add("Home Page title is: " + homePage.getPageTitle());
+        brandPage.navigateTo("https://autoportal.com/newcars/honda/");
+        pageTitles.add("Brand Page title is: " + brandPage.getPageTitle());
+        brochurePage.navigate();
+        pageTitles.add(("Brochure Page title is: " + brochurePage.getPageTitle()));
+        onRoadPricePage.navigate();
+        pageTitles.add(("OnroadPrice Page title is: " + onRoadPricePage.getPageTitle()));
+        modelPage.navigateTo("https://autoportal.com/newcars/honda/wrv/");
+        pageTitles.add("Model Page title is: " + modelPage.getPageTitle());
+        storePageTitlesInFile(pageTitles.toString());
+    }
+
+    @Override
+    @AfterMethod
+    public void afterMethodStop() {
+        super.afterMethodStop();
+    }
+}
